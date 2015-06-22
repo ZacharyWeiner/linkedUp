d6578f5dce41c584702b21dcf92e32d61899f1f2
ZacharyWeiner <zach@cloud.com>
2015-06-22 02:36:53 -0400

    added db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+User.create(email: "zack1@mbadojo.com", password: "password", username: "zw12345", password_confirmation:"password")
+User.create(email: "zack2@mbadojo.com", password: "password", username: "zw12346", password_confirmation:"password")
+User.create(email: "zack3@mbadojo.com", password: "password", username: "zw12347", password_confirmation:"password")
+User.create(email: "zack4@mbadojo.com", password: "password", username: "zw12348", password_confirmation:"password")
+User.create(email: "zack5@mbadojo.com", password: "password", username: "zw12349", password_confirmation:"password")
+
+
+p "Test users creates"
