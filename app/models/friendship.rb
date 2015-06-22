ebc7d7ae3a13cdd36b6cd38cad29cd37f5e63795
ZacharyWeiner <zach@cloud.com>
2015-06-22 03:06:38 -0400

    added friendships

---
+++ app/models/friendship.rb
@@ -0,0 +1,20 @@
+class Friendship < ActiveRecord::Base
+	belongs_to :user
+	belongs_to :friend, class_name: "User"
+
+#Accept 
+
+	def accept_friendship
+		self.update_attributes(state: "active", friended_at: Time.now)
+	end
+
+#Deny 
+	def deny _friendship
+		self.destroy
+	end
+
+#Cancel 
+	def cancel_friendship
+		self.destroy
+	end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ActiveRecord::Base
 
          validates_presence_of :username
          validates_uniqueness_of :username
+
+    has_many :friendships, dependent: :destroy
+
+    #needs to be able to find all people that requested to be our friend 
+    has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id", dependent: :destroy
 end
